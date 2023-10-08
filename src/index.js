ef2309e82841d2533dd27042b0e843a874e5433c
ilyanishtun <inishtun@yandex.ru>
2023-10-09 02:54:02 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let string = n.toString();
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    result = string[i] + result;
+  }
+  resultNumber = parseInt(result)
+  return resultNumber;
 }
+
+
+
